060d0f02301d1ed8f6a1b0b4a84e8d1209c39fce
Marcelo Vieira <marcelo@informatico.com.br>
2021-01-27 13:59:16 -0300

    Commit ao final da segunda aula

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizContainer from '../src/components/QuizContainer';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -16,17 +18,6 @@ import GitHubCorner from '../src/components/GitHubCorner';
 //   background-size: cover;
 //   background-position: center;
 // `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -53,21 +44,21 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
-            <form onSubmit={function (e) {
+            <form onSubmit={(e) => {
               e.preventDefault();
 
               router.push(`/quiz?name=${name}`);
             }}
             >
-              <input
-                onChange={function (e) {
-                  setName(e.target.value);
-                }}
+              <Input
+                name="nameOfUser"
+                onChange={(e) => setName(e.target.value)}
                 placeholder="What's your name?"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Let&lsquo;s play
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {'Let\'s play!'}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
