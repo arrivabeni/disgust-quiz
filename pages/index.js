63c90abc7afb1c1b96de46541572409ebdb587b5
Marcelo Vieira <marcelo@informatico.com.br>
2021-01-26 23:18:35 -0300

    Commit after second class

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -25,8 +29,22 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title}</title>
+        <meta name="title" content={db.title} />
+        <meta name="description" content={db.site_description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={db.url} />
+        <meta property="og:title" content={db.title} />
+        <meta property="og:description" content={db.site_description} />
+        <meta property="og:image" content={db.og_image} />
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -35,6 +53,22 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={function (e) {
+              e.preventDefault();
+
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (e) {
+                  setName(e.target.value);
+                }}
+                placeholder="What's your name?"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Let&lsquo;s play
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
